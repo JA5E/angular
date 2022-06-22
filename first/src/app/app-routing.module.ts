85a04ba9e9739818cda6bc2bb5a40da720132f04
JASE <JA5E@jase0485>
2022-06-22 10:15:30 -0500

    routingWorking

--- first/src/app/app-routing.module.ts
+++ first/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {Routes} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
